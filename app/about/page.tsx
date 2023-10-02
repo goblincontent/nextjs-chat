0c029a5c3f344a9a806e3a877ffad7ec4af18e73
Content Goblin <goblincontent@gmail.com>
2023-10-03 06:47:13 +1100

    changed about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,10 @@ export default function About() {
         <div className="mx-auto max-w-2xl px-4">
             <div className="rounded-lg border bg-background p-8">
                 <h1 className="mb-2 text-lg font-semibold">
-                    hello
+                    This Demo is an example to showcase the power of RAG (Retrieval-Augmented Generation)
+                    though the use of a custom AI Chatbot designed for lawyers to do case research.
+                    It uses a supabase postgres database backend to store embeddings for chunks of case law text.
+                    Each chunk consists of roughly 400 tokens.
                 </h1>
             </div>
         </div>
